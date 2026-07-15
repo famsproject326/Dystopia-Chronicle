13bf5c65c207ab10236d85b5e06856e9ac3d0481
famsproject326 <famsproject326@gmail.com>
2026-07-16 08:48:55 +0900

    state.js を更新

--- state.js
+++ state.js
@@ -1,1 +1,13 @@
-あ
+// ======================================
+// Project Dystopia Chronicle
+// state.js
+// ゲームの状態管理
+// ======================================
+
+const gameState = {
+    mode: "EXPLORE",      // "EXPLORE", "STORY", "HACKING"
+    roomIndex: 2,         // 現在の部屋
+    chapterIndex: 0,      // 現在の章
+    storyIndex: 0,        // 現在のストーリー行
+    inputCode: []         // 入力中のコード
+};
