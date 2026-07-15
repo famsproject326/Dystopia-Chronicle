bb566b9357ae736fc1f949f19d23a4696ef9292b
famsproject326 <famsproject326@gmail.com>
2026-07-15 09:44:02 +0900

    keypad.js を更新

--- keypad.js
+++ keypad.js
@@ -2,12 +2,16 @@ function drawKeypad(ctx, width, height) {
     ctx.fillStyle = "#0f0";
     ctx.font = "25px 'Courier New'";
     ctx.textAlign = "center";
-    ctx.fillText("ENTER 4-DIGIT CODE", width / 2, height / 4);
+    ctx.fillText("ENTER 4-DIGIT CODE", width / 2, height / 2 - 150);
+
+    const startX = (width / 2) - 80;
+    const startY = (height / 2) - 80;
 
     for (let i = 0; i < 9; i++) {
-        let x = (width / 2) - 60 + (i % 3) * 60;
-        let y = (height / 2) - 60 + Math.floor(i / 3) * 60;
+        let x = startX + (i % 3) * 60;
+        let y = startY + Math.floor(i / 3) * 60;
         ctx.strokeStyle = "#0f0";
+        ctx.lineWidth = 2;
         ctx.strokeRect(x, y, 50, 50);
         ctx.fillText((i + 1).toString(), x + 25, y + 35);
     }
