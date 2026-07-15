fba822f78a08b04aa0f8f8d6a79d4c40144385b8
famsproject326 <famsproject326@gmail.com>
2026-07-16 07:03:56 +0900

    save.js を更新

--- save.js
+++ save.js
@@ -1,1 +1,75 @@
-あ
+// ======================================
+// Project Dystopia Chronicle
+// save.js
+// セーブ・ロード
+// ======================================
+
+
+// ======================================
+// セーブ
+// ======================================
+
+function saveGame(){
+
+    const saveData = {
+
+        chapterIndex,
+
+        storyIndex,
+
+        roomIndex,
+
+        player: {
+
+            x: player.x,
+            y: player.y
+
+        },
+
+        flags
+
+    };
+
+    localStorage.setItem(
+        SAVE_KEY,
+        JSON.stringify(saveData)
+    );
+
+}
+
+
+// ======================================
+// ロード
+// ======================================
+
+function loadGame(){
+
+    const data = localStorage.getItem(SAVE_KEY);
+
+    if(!data) return false;
+
+    const saveData = JSON.parse(data);
+
+    chapterIndex = saveData.chapterIndex;
+    storyIndex = saveData.storyIndex;
+    roomIndex = saveData.roomIndex;
+
+    player.x = saveData.player.x;
+    player.y = saveData.player.y;
+
+    Object.assign(flags, saveData.flags);
+
+    return true;
+
+}
+
+
+// ======================================
+// セーブ削除
+// ======================================
+
+function deleteSave(){
+
+    localStorage.removeItem(SAVE_KEY);
+
+}
