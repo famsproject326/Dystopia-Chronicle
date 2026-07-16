6472e004304afd2107768c50b120b3fe1c48e9f1
famsproject326 <famsproject326@gmail.com>
2026-07-16 10:02:38 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// ======================================
+// Project Dystopia Chronicle
+// script.js
+// ======================================
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const dpr = window.devicePixelRatio || 1;
@@ -9,19 +14,26 @@ ctx.scale(dpr, dpr);
 
 const roomNames = ["302", "303", "ロビー", "301"];
 
+function loop(){
+    if(gameState.mode === "EXPLORE") updatePlayer();
+    
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    
+    switch(gameState.mode){
+        case "EXPLORE": 
+            drawExplore(ctx, roomNames[gameState.roomIndex] || "UNKNOWN"); 
+            break;
+        case "HACKING": 
+            drawHack(ctx, gameState.inputCode); 
+            break;
+    }
+    requestAnimationFrame(loop);
+}
+
 function init(){
     loadStage();
     resetPlayer();
     requestAnimationFrame(loop);
 }
 
-function loop(){
-    if(gameState.mode === "EXPLORE") updatePlayer();
-    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    switch(gameState.mode){
-        case "EXPLORE": drawExplore(ctx, roomNames[gameState.roomIndex]); break;
-        case "HACKING": drawHack(ctx, gameState.inputCode); break;
-    }
-    requestAnimationFrame(loop);
-}
 init();
