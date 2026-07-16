8781d7963785041f1cda75a52fbc2aee3c7b7eb5
famsproject326 <famsproject326@gmail.com>
2026-07-16 10:12:22 +0900

    ui.js を更新

--- ui.js
+++ ui.js
@@ -1,10 +1,9 @@
 function drawExplore(ctx, roomName) {
-    ctx.fillStyle = "#ffffff"; ctx.textAlign = "center";
+    ctx.fillStyle = "#ffffff"; ctx.font = "20px Courier New"; ctx.textAlign = "center";
     ctx.fillText("ROOM : " + roomName, window.innerWidth / 2, 50);
     drawPlayer(ctx);
 }
 function drawHack(ctx, inputCode) {
     ctx.fillStyle = "rgba(0,0,0,0.95)"; ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
     ctx.fillStyle = "#ffffff"; ctx.fillText("CODE : " + inputCode.join(""), window.innerWidth / 2, 100);
-    drawKeypad(ctx, window.innerWidth, window.innerHeight);
 }
